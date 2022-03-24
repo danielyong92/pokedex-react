c8b50c147915466c015ea6577749752b4ecdc6a4
hz298786@hertz.net <hz298786@hertz.net>
2022-03-24 10:04:45 -0500

    added css and imported MUI

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+// const styles = require("./Card.module.scss");
+import Grid from "@mui/material/Grid";
 
 export const Card = props => {
   const { id, name, image, type, index } = props;
+
   return (
     <>
-      <div>
+      <div class="cardContainer">
         <img id="image" src={image}></img>
         <div>Pokemon Name: {name}</div>
         <div>Id: {id}</div>
--- src/CardRow/CardRow.jsx
+++ src/CardRow/CardRow.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import Card from "../Card/Card";
+import Grid from "@mui/material/Grid";
 
-export const CardRow = (props) => {
+export const CardRow = props => {
   const { pokemonData } = props;
-    
+
   return (
     <>
-    {console.log("pokemon, ", pokemonData)}
-    {pokemonData.map((pokemon,index) => 
-      <Card id={pokemon.id} name={pokemon.name} type={pokemon.types[0].type.name} image={pokemon.sprites.front_default} key={index}/>
-    )}
+      {console.log("pokemon, ", pokemonData)}
+      <Grid container>
+        {pokemonData.map((pokemon, index) => (
+          <Grid item md={4} direction={"column"}>
+            <Card
+              id={pokemon.id}
+              name={pokemon.name}
+              type={pokemon.types[0].type.name}
+              image={pokemon.sprites.other.dream_world.front_default}
+              key={index}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </>
   );
 };
